f91d7dd2f9ab69a1258cb81005481a3bf24ba121
Luke <work37@gmail.com>
2015-11-30 03:05:55 +0300

    a lot of todo

--- src/Parser.java
+++ src/Parser.java
@@ -1,7 +1,7 @@
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import com.sun.deploy.net.proxy.StaticProxyManager;
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -10,6 +10,9 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 import java.util.Random;
 
+//TODO reconnect refactoring
+//TODO make a links list first, start from previous
+//TODO track updates (on firs page ?) or hash
 
 public class Parser {
     static long counter=0;
@@ -20,7 +23,8 @@ public class Parser {
 
     public static void main(String[] args) throws IOException {
         connectToDataBase();
-        parseSongs("http://amdm.ru/");
+//        parseSongs("http://amdm.ru/");
+        getLinksOfArtists("http://amdm.ru/chords/1/");
     }
 
     private static void connectToDataBase() {
@@ -30,7 +34,8 @@ public class Parser {
     }
 
     private static void parseSongs(String url) throws IOException {
-        Document doc = Jsoup.connect(url).get();
+        Connection con = connect(url);
+        Document doc = con.get();
         Elements linksOfArtistsAlphabet = doc.select(".alphabet a");
         for (Element link : linksOfArtistsAlphabet) {
             getLinksOfArtists(link.attr("abs:href"));
@@ -38,20 +43,17 @@ public class Parser {
     }
 
     private static void getLinksOfArtists(String url) throws IOException {
-        Document doc = Jsoup.connect(url).get();
+        Connection con = connect(url);
+        Document doc = con.get();
         Elements linksOfArtists = doc.select("article .artist");
         for (Element link : linksOfArtists) {
             getLinksOfSongs( link.attr("abs:href") );
-            try {
-                Thread.sleep(rand.nextInt(10000)+10000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
         }
     }
 
     private static void getLinksOfSongs(String url) throws IOException {
-        Document doc = Jsoup.connect(url).get();
+        Connection con = connect(url);
+        Document doc = con.get();
         Elements linksOfSongs = doc.select(".artist-profile-song-list .g-link");
         for (Element link : linksOfSongs) {
             getSong(link.attr("abs:href"));
@@ -59,7 +61,8 @@ public class Parser {
     }
 
     private static void getSong(String url) throws IOException {
-        Document doc = Jsoup.connect(url).get();
+        Connection con = connect(url);
+        Document doc = con.get();
         String artist = doc.select("[itemprop=\"byArtist\"]").first().text();
         String songName = doc.select("[itemprop=\"name\"]").first().text();
         String lyric = doc.select("[itemprop=\"chordsBlock\"]").first().text();
@@ -69,7 +72,7 @@ public class Parser {
         addSongToDatabase(artist, songName, lyric);
 
         try {
-            Thread.sleep(rand.nextInt(1000));
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -82,5 +85,10 @@ public class Parser {
                 .append("lyric", lyric);
 
         songs.insertOne(songDocument);
+    }
+
+    private static Connection connect(String url){
+        Connection con = Jsoup.connect(url).userAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.21 (KHTML, like Gecko) Chrome/19.0.1042.0 Safari/535.21").timeout(10000);
+        return con;
     }
 }
